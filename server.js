b69c1c54d734d7572e574d780d9b093e6e51c69d
Tamara <tamara_almanaseer@live.com>
2021-04-03 14:56:48 +0300

    2nd iteration

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express=require('express');
 
 const server= express();
 const PORT= 3000;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT||3000;
 
 //route definitions
 //localhost:3000/test
