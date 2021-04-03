2cfb011f9cd080c85d81b1661014fd2f5dfa633a
Tamara <tamara_almanaseer@live.com>
2021-04-03 15:06:09 +0300

    3rd iteration

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT||3000;
 
 server.use(express.static('./public'));
 
+
+server.get('/',(req,res)=>{
+    res.send('home route')
+})
 server.get('/test', (request, response)=>{
 response.send('you server');
 })
